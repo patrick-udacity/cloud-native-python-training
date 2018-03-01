0f98fcedcd47c14bc9d3309d7697d1f9baa5dc00
Patrick W Parkison <patrick.udacity@gmail.com>
2018-03-01 16:50:39 -0600

    Working on unit testing.

--- app.py
+++ app.py
@@ -75,6 +75,55 @@ def list_userID(user_id):
     conn.close()
     return jsonify(user)
 
+def add_user(new_user):
+    conn = sqlite3.connect(dbName)
+    print ("Opened database successfully");
+    cursor=conn.cursor()
+    cursor.execute("SELECT * from users where username=? or email=?",(new_user['username'],new_user['email']))
+    data = cursor.fetchall()
+    if len(data) != 0:
+        abort(409)
+    else:
+       cursor.execute("insert into users (username, email, password, full_name) values(?,?,?,?)",(new_user['username'],new_user['email'], new_user['password'], new_user['name']))
+       conn.commit()
+       return "Success"
+    conn.close()
+    return jsonify(new_user)
+
+
+def del_user(del_user):
+    conn = sqlite3.connect(dbName)
+    print ("Opened database successfully");
+    cursor=conn.cursor()
+    cursor.execute("SELECT * from users where username=? ",(del_user,))
+    data = cursor.fetchall()
+    print ("Data" ,data)
+    if len(data) == 0:
+        abort(404)
+    else:
+       cursor.execute("delete from users where username==?",(del_user,))
+       conn.commit()
+       return "Success"
+
+def upd_user(user):
+    conn = sqlite3.connect(dbName)
+    print ("Opened database successfully");
+    cursor=conn.cursor()
+    cursor.execute("SELECT * from users where id=? ",(user['id'],))
+    data = cursor.fetchall()
+    print (data)
+    if len(data) == 0:
+        abort(404)
+    else:
+        key_list=user.keys()
+        for i in key_list:
+            if i != "id":
+                print (user, i)
+                # cursor.execute("UPDATE users set {0}=? where id=? ", (i, user[i], user['id']))
+                cursor.execute("""UPDATE users SET {0} = ? WHERE id = ?""".format(i), (user[i], user['id']))
+                conn.commit()
+    return "Success"
+
 #Url decorators
 
 #Retrieve information on the API version.
@@ -108,6 +157,39 @@ def get_user(matchStr):
 @app.route('/api/v1/users/<int:user_id>', methods=['GET'])
 def get_userID(user_id):
     return list_userID(user_id)
+
+@app.route('/api/v1/users', methods=['POST'])
+def create_user():
+    if not request.json or not 'username' in request.json or not 'email' in request.json or not 'password' in request.json:
+        abort(400)
+    user = {
+        'username': request.json['username'],
+        'email': request.json['email'],
+        'name': request.json.get('name',""),
+        'password': request.json['password']
+    }
+    return jsonify({'status': add_user(user)}), 201
+
+@app.route('/api/v1/users', methods=['DELETE'])
+def delete_user():
+    if not request.json or not 'username' in request.json:
+        abort(400)
+    user=request.json['username']
+    return jsonify({'status': del_user(user)}), 200
+
+
+@app.route('/api/v1/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = {}
+    # if not request.json:
+    #     abort(400)
+    user['id']=user_id
+    key_list = request.json.keys()
+    for i in key_list:
+        user[i] = request.json[i]
+    print (user)
+
+    return jsonify({'status': upd_user(user)}), 200
 
 #Error handlers
 
